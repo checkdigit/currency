a1ed37e61a18e5efb8eac99fc426d2b299717663
Carl Ansley <carl.ansley@gmail.com>
2020-01-27 17:51:43 -0700

    CURRENCY-1 added new functionality, noEmit=false

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -2,10 +2,81 @@
 
 import * as assert from 'assert';
 
-import currency from './index';
+import * as currency from './index';
 
 describe('currency', () => {
-  it('works', () => {
-    assert.strictEqual(currency(), 2);
+  it('minorUnitDigits returns correct number for each currency', () => {
+    assert.strictEqual(currency.getMinorUnitDigits('USD'), 2);
+    assert.strictEqual(currency.getMinorUnitDigits('JPY'), 0);
+  });
+
+  it('getSymbol', () => {
+    assert.strictEqual(currency.getSymbol('USD'), '$');
+    assert.strictEqual(currency.getSymbol('JPY'), '¥');
+  });
+
+  it('format', () => {
+    assert.strictEqual(currency.format({ amount: '-1230', currency: 'USD' }), '-$12.30');
+    assert.strictEqual(currency.format({ amount: BigInt(123456), currency: 'USD' }), '$1,234.56');
+    assert.strictEqual(
+      currency.format({ amount: BigInt('123456789012345678901234567890'), currency: 'USD' }),
+      '$1,234,567,890,123,456,789,012,345,678.90'
+    );
+    assert.strictEqual(
+      currency.format(
+        { amount: '123456', currency: 'USD' },
+        {
+          hideGrouping: true
+        }
+      ),
+      '$1234.56'
+    );
+    assert.strictEqual(
+      currency.format(
+        { amount: '123456', currency: 'USD' },
+        {
+          hideCurrency: true,
+          hideGrouping: true
+        }
+      ),
+      '1234.56'
+    );
+    assert.strictEqual(
+      currency.format(
+        { amount: '123456', currency: 'USD' },
+        {
+          hideCurrency: true,
+          hideGrouping: true,
+          hideDecimal: true
+        }
+      ),
+      '123456'
+    );
+    assert.throws(() =>
+      currency.format(
+        { amount: '123456', currency: 'USD' },
+        {
+          hideDecimal: true
+        }
+      )
+    );
+    assert.throws(() =>
+      currency.format(
+        { amount: '123456', currency: 'USD' },
+        {
+          hideGrouping: true,
+          hideDecimal: true
+        }
+      )
+    );
+    assert.throws(() =>
+      currency.format(
+        { amount: '123456', currency: 'USD' },
+        {
+          hideCurrency: true,
+          hideDecimal: true
+        }
+      )
+    );
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,65 @@
 // index.ts
 
-export default () => {
+export type Amount = string | bigint;
+export type Currency = 'USD' | 'JPY';
+
+export interface Money {
+  amount: Amount;
+  currency: Currency;
+}
+
+interface CurrencyFormatOptions {
+  hideCurrency?: boolean;
+  hideGrouping?: boolean;
+  hideDecimal?: boolean;
+}
+
+export function format(
+  { amount, currency }: Money,
+  options?: CurrencyFormatOptions,
+  locales?: string | string[]
+): string {
+  const amountInteger = typeof amount === 'bigint' ? amount : BigInt(amount);
+
+  const majorUnitAmount = amountInteger / BigInt('100');
+  const minorUnitAmount = (amountInteger < BigInt(0) ? -amountInteger : amountInteger) % BigInt('100');
+
+  if (options?.hideDecimal && !(options?.hideCurrency && options?.hideGrouping)) {
+    throw Error('hideDecimal can only be true if hideCurrency and hideGrouping are also true');
+  }
+
+  return Intl.NumberFormat(locales, { style: 'currency', currency })
+    .formatToParts((majorUnitAmount as unknown) as number)
+    .reduce((previous: string, current) => {
+      switch (current.type) {
+        case 'minusSign':
+          return previous + current.value;
+        case 'currency':
+          return previous + (options?.hideCurrency ? '' : current.value);
+        case 'integer':
+          return previous + current.value;
+        case 'group':
+          return previous + (options?.hideGrouping ? '' : current.value);
+        case 'decimal':
+          return previous + (options?.hideDecimal ? '' : current.value);
+        case 'fraction':
+          return previous + minorUnitAmount.toString();
+        default:
+          throw Error(`${current.type} unsupported`);
+      }
+    }, '');
+}
+
+export function getSymbol(currency: Currency, locales?: string | string[]): string | undefined {
+  return Intl.NumberFormat(locales, { style: 'currency', currency })
+    .formatToParts(0)
+    .filter(part => part.type === 'currency')[0]?.value;
+}
+
+export function getMinorUnitDigits(currency: Currency, locales?: string | string[]) {
   return (
-    // eslint-disable-next-line no-undefined
-    Intl.NumberFormat(undefined, { style: 'currency', currency: 'USD' })
+    Intl.NumberFormat(locales, { style: 'currency', currency })
       .formatToParts(0)
       .filter(part => part.type === 'fraction')[0]?.value.length ?? 0
   );
-};
+}
