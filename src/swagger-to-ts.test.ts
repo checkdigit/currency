a2c9b708b23d6a527f2fefa78a8828db031504e7
Le Cong <le.cong@checkdigit.com>
2025-09-24 03:07:27 -0400

    Refactor prepare scripts in package.json and update TypeReferenceModel in index.tsp for clarity; enhance type extraction in swagger-to-ts.test.ts

--- src/swagger-to-ts.test.ts
+++ src/swagger-to-ts.test.ts
@@ -1,6 +1,7 @@
 // swagger-to-ts.test.ts
 
 import { promises as fs } from 'node:fs';
+import { strict as assert } from 'node:assert';
 
 import dtsgen, { type Options as DtsgenOptions, type JsonSchema, parseSchema } from 'dtsgenerator';
 import yaml from 'js-yaml';
@@ -12,15 +13,19 @@ const dtsgenFunction =
       (dtsgen.default as (options: DtsgenOptions) => Promise<string>);
 
 function extractTypes(swaggerSchema: string): string {
-  const namespaceWrapperEndingIndex = `declare namespace Components {
-    namespace Schemas {`.length;
-  const sampleModelBeginningIndex = swaggerSchema.indexOf('export interface SampleModel');
-
-  return swaggerSchema.slice(namespaceWrapperEndingIndex, sampleModelBeginningIndex);
+  const regex =
+    // Use a regular expression to extract the content between the namespace wrapper and TypeReferenceModel interface
+    // eslint-disable-next-line sonarjs/slow-regex
+    /declare\s*namespace\s*Components\s*\{\s*namespace\s*Schemas\s*\{(?<types>[\s\S]*)\s*export\s*interface\s*TypeReferenceModel/u;
+  const match = swaggerSchema.match(regex);
+  assert.ok(match?.[1], 'Failed to extract types from the generated schema.');
+  return match[1].trim();
 }
 
 async function generateTypesFromSwagger() {
-  const swaggerContents = await fs.readFile('./src/swagger.yml', 'utf8');
+  const [_node, _file, inputPath, outputPath] = process.argv;
+
+  const swaggerContents = await fs.readFile(inputPath ?? './src/swagger.yml', 'utf8');
 
   const swaggerYaml = yaml.load(swaggerContents);
   const swaggerSchema = await dtsgenFunction({
@@ -28,7 +33,7 @@ async function generateTypesFromSwagger() {
   });
 
   const types = extractTypes(swaggerSchema);
-  await fs.writeFile('./src/swagger.ts', types, 'utf8');
+  await fs.writeFile(outputPath ?? './src/swagger.ts', types, 'utf8');
 }
 
 await generateTypesFromSwagger();
