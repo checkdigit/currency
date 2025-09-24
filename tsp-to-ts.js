c054a6a829e69877361b14306f7509913ca93080
Le Cong <le.cong@checkdigit.com>
2025-09-24 15:58:23 -0400

    change tsp-ts convertor as js due to typespec's typing issue

--- tsp-to-ts.js
+++ tsp-to-ts.js
@@ -3,8 +3,8 @@
 import { promises as fs } from 'node:fs';
 import { strict as assert } from 'node:assert';
 
-import { compile, logDiagnostics, NodeHost, type Program } from '@typespec/compiler';
-import dtsgen, { type Options as DtsgenOptions, type JsonSchema, parseSchema } from 'dtsgenerator';
+import { compile, logDiagnostics, NodeHost } from '@typespec/compiler';
+import dtsgen, { parseSchema } from 'dtsgenerator';
 import yaml from 'js-yaml';
 
 const NAMESPACE = 'Currency';
@@ -15,13 +15,9 @@ const GENERATED_SERVICE_TSP_PATH = `${GENERATED_DIR}/service.tsp`;
 const GENERATED_SWAGGER_FILENAME = 'swagger.yml';
 const DEFAULT_OUTPUT_TYPES_PATH = './src/swagger.ts';
 
-const dtsgenFunction =
-  typeof dtsgen === 'function'
-    ? dtsgen
-    : // @ts-expect-error, due to the possible package bundling issue of 3rd party library, we need to use the default export inside the default export
-      (dtsgen.default as (options: DtsgenOptions) => Promise<string>);
+const dtsgenFunction = typeof dtsgen === 'function' ? dtsgen : dtsgen.default;
 
-async function compileTsp(inputTspPath: string) {
+async function compileTsp(inputTspPath) {
   const program = await compile(NodeHost, inputTspPath);
   // Print any diagnostics just like the CLI does
   if (program.diagnostics.length > 0) {
@@ -31,7 +27,7 @@ async function compileTsp(inputTspPath: string) {
   return program;
 }
 
-async function generateServiceTsp(program: Program) {
+async function generateServiceTsp(program) {
   const enumReferences = [...program.getGlobalNamespaceType().enums.keys()]
     .map((enumName) => `  ${enumName}: ${enumName};`)
     .join('\n');
@@ -64,21 +60,20 @@ async function compileServiceTspAndGenerateSwagger() {
   await compile(NodeHost, GENERATED_SERVICE_TSP_PATH, { emit, options });
 }
 
-function extractTypes(swaggerSchema: string): string {
+function extractTypes(swaggerSchema) {
   const regex =
     // Use a regular expression to extract the content between the namespace wrapper and TypeReferenceModel interface
-    // eslint-disable-next-line sonarjs/slow-regex
     /declare\s*namespace\s*Components\s*\{\s*namespace\s*Schemas\s*\{(?<types>[\s\S]*)\s*export\s*interface\s*TypeReferenceModel/u;
   const match = swaggerSchema.match(regex);
   assert.ok(match?.groups?.['types'], 'Failed to extract types from the generated schema.');
   return match.groups['types'].trim();
 }
 
-async function generateTypesFromSwagger(outputTypesPath: string) {
+async function generateTypesFromSwagger(outputTypesPath) {
   const swaggerContents = await fs.readFile(`${GENERATED_DIR}/${GENERATED_SWAGGER_FILENAME}`, 'utf8');
   const swaggerYaml = yaml.load(swaggerContents);
   const swaggerSchema = await dtsgenFunction({
-    contents: [parseSchema(swaggerYaml as JsonSchema)],
+    contents: [parseSchema(swaggerYaml)],
   });
 
   const types = extractTypes(swaggerSchema);
@@ -91,6 +86,7 @@ async function main() {
 
   // prepare the generated directory
   await fs.rm(GENERATED_DIR, { recursive: true, force: true });
+  await fs.rm(outputTypesPath, { force: true });
   await fs.mkdir(GENERATED_DIR, { recursive: true });
 
   const program = await compileTsp(inputTspPath);
